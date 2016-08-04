47f6cf99f827815665fa98984a25a4f757649261
Salazar, Héctor <hector.salazar@autodesk.com>
2016-08-04 16:51:50 -0500

    Fixed typo, tests passing

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ActiveRecord::Base
   belongs_to :project
+
+  validates :title,           presence: true
+  validates :description,     presence: true
+  validates :urgency,         numericality: {
+                                    only_integer: true,
+                                    greater_than: 0,
+                                    less_than_or_equal_to: 5
+                                    }
 end
