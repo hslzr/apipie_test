766190db723c1a7dce1818f64db3d307da0c2098
Salazar, Héctor <hector.salazar@autodesk.com>
2016-08-04 18:42:44 -0500

    Modified apipie initializer

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,20 @@
 Apipie.configure do |config|
   config.app_name                = "ApipieTest"
-  config.api_base_url            = "/api"
-  config.doc_base_url            = "/apipie"
+  config.api_base_url            = "/"
+  config.doc_base_url            = "/api_doc"
+  config.copyright               = "&copy; #{Time.now.year} made with \xE2\x9D\xA4 by hslzr"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  
+  
+  config.reload_controllers = Rails.env.development?
+  config.app_info["1.0"] = "
+  This is a dummy app. This is where you'd put general information about your api.
+  "
+
+  config.authenticate = Proc.new do
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'demo' && password == 'demo'
+    end
+  end
 end
