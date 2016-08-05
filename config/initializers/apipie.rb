f01b9d47ca456511f42ac6a044093856369f53dc
Héctor Salazar <h.salazar509@gmail.com>
2016-08-05 00:56:53 -0500

    Added emoji heart. This is important because reasons

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,11 +2,11 @@ Apipie.configure do |config|
   config.app_name                = "ApipieTest"
   config.api_base_url            = "/"
   config.doc_base_url            = "/api_doc"
-  config.copyright               = "&copy; #{Time.now.year} made with \xE2\x9D\xA4 by hslzr"
+  config.copyright               = "&copy; #{Time.now.year} made with ❤️ by " + "<a href='https://github.com/hslzr' style='text-decoration: none;'>hslzr</a>".html_safe
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
-  
-  
+
+
   config.reload_controllers = Rails.env.development?
   config.app_info["1.0"] = "
   This is a dummy app. This is where you'd put general information about your api.
